bf74069051b2d45dc20a06503ace31a3a5fd2a2a
Sun-pc\Sash <krialsash@gmail.com>
2020-12-02 14:24:08 +0200

    sravnennie

---
+++ sravnenie-2h-chisel/sravnenie-2h-chisel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sravnenie_2h_chisel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 13;
+            int b = 45;
+            int c = 37;
+            int max = -1;
+            max = a;
+
+            if (max < b)
+            {
+                max = b;
+            }
+            if (max < c)
+            {
+                max = c;
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
